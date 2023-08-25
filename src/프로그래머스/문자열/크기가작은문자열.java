7e48f2b95c86e253344cdd1f4b724353d41ea7c3
june0216 <ziyun1612@ewhain.net>
2023-08-25 23:04:35 +0900

    :sparkles: [PRO] 문자열처리

---
+++ src/프로그래머스/문자열/크기가작은문자열.java
@@ -0,0 +1,15 @@
+package 프로그래머스.문자열;
+
+public class 크기가작은문자열 {
+	public int solution(String t, String p) {
+		int answer = 0;
+
+		for(int i = 0; i < t.length()-p.length()+1; i++){
+			long temp = Long.parseLong(t.substring(i, i + p.length()));
+			if(Long.valueOf(temp) <= Long.valueOf(p)){
+				answer +=1;
+			}
+		}
+		return answer;
+	}
+}
